8dee3c354b4ff5ccd62f9b3951a5095d37f62434
Kirill Chernov <sireler@gmail.com>
2022-04-11 18:21:14 +0300

    comment BaseEntity

--- src/main/java/com/sireler/quiz/model/BaseEntity.java
+++ src/main/java/com/sireler/quiz/model/BaseEntity.java
@@ -7,9 +7,18 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+/**
+ * Simple domain object with id, created_at, updated_at properties.
+ * Used as a base class for objects needing these properties.
+ */
 @MappedSuperclass
 @Getter
 @Setter
